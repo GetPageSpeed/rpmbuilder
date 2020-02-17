2ddbe2fe1fbdfeac92156ca34b4ab5865a8437b1
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2020-02-17 12:38:52 +0300

    Up

---
+++ assets/rpmbuilder_ua.py
@@ -0,0 +1,9 @@
+import dnf
+
+
+class RpmBuilderUa(dnf.Plugin):
+    name = 'rpmbuilder_ua'
+
+    def config(self):
+        repos = self.base.repos.get_matching('getpagespeed-*')
+        repos.set_http_headers(["User-Agent: rpmbuilder"])
