3613d76335df4399276fa9056fe511c8ae8c105a
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2020-12-10 13:38:45 +0300

    f1

--- assets/distfix.sh
+++ assets/distfix.sh
@@ -4,12 +4,12 @@ if [[ $(rpm -E %{rhel}) == 6 ]]; then
   curl https://www.getpagespeed.com/files/centos6-eol.repo --output /etc/yum.repos.d/CentOS-Base.repo
   yum -y install yum-plugin-ovl
   yum -y install centos-release-scl
-  yum -y install yum-plugin-versionlock
-  yum -y install epel-release
-  yum versionlock yum yum-utils epel-release
-  curl https://www.getpagespeed.com/files/centos6-epel-eol.repo --output /etc/yum.repos.d/epel.repo
   curl https://www.getpagespeed.com/files/centos6-scl-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl.repo
   curl https://www.getpagespeed.com/files/centos6-scl-rh-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+  yum -y install yum-plugin-versionlock
+  yum -y install epel-release
+  curl https://www.getpagespeed.com/files/centos6-epel-eol.repo --output /etc/yum.repos.d/epel.repo
+  yum versionlock yum yum-utils epel-release
 fi
 
 sed -ri 's@^default_grabber\.opts\.user_agent\s+.*@default_grabber.opts.user_agent = "rpmbuilder"@' /usr/lib/python2.7/site-packages/yum/__init__.py  ||:
