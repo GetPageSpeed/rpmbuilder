277ffd2f6d864ec6b0a0f3d89eb6627d23f225c1
Danila Vershinin <ciapnz@gmail.com>
2023-09-25 15:23:37 +0700

    Testing failure OK, fixed #1

--- assets/transient/user-agent.py
+++ assets/transient/user-agent.py
@@ -5,5 +5,5 @@ class RpmBuilderUa(dnf.Plugin):
     name = 'rpmbuilder_ua'
 
     def config(self):
-        repos = self.base.repos.get_matching('getpagespeed-*')
+        repos = self.base.repos.get_matching('*getpagespeed*')
         repos.set_http_headers(["User-Agent: XXXXXXXXXX"])
